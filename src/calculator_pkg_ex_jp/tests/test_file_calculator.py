c30f9c47ca4d9b2fd51f4683b85bcae5ae9017c9
JimInput <oJimmy05o@gmail.com>
2025-11-18 14:09:50 -0500

    Pybind11

--- src/calculator_pkg_ex_jp/tests/test_file_calculator.py
+++ src/calculator_pkg_ex_jp/tests/test_file_calculator.py
@@ -1,6 +1,11 @@
 from calculator_pkg_ex_jp.file_calculator import FileCalculator
+from calculator_pkg_ex_jp._core import CPPFileCalculator
 # from ..file_calculator import FileCalculator
 
 
 def test_file_calculator():
     assert FileCalculator().sum_file() == 6
+    assert CPPFileCalculator().sum_file() == 6
+
+
+
